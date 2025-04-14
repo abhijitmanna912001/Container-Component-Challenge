b9e39e4a622cddf121992e735dcf5285b0ffc337
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-04-14 18:00:52 +0530

    Data Source Component Rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import DataSource from "./components/shared/DataSource";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="mt-[20px]">
+      <h1>Welcome to the Fake Store</h1>
+      <DataSource
+        getDataFunc={getServerData("https://fakestoreapi.com/products")}
+        resourceName="products"
+      ></DataSource>
+    </div>
+  );
 };
 
 export default App;
